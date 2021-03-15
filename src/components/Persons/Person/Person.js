71cb149f90fc9adf9386ffe3de21846a26e0c8b4
Jen <medtrakdeveloper1@MedTrakDeveloper1s-MacBook-Pro.local>
2021-03-14 22:27:09 -0400

    use built in React.Fragment instead of aux to wrap Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,17 +2,16 @@
 
 import React, {Component} from 'react';
 import classes from './Person.css';
-import Aux from '../../../hoc/Aux.js'
 
 class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering');
 		return(
-			<Aux>
+			<React.Fragment>
 			<p onClick={this.props.click}>I'm {this.props.name}, I'm {this.props.age} years old, and I have {Math.floor(Math.random() * 30)} pets.</p>
 			<p>{this.props.children}</p>
 			<input type="text" onChange={this.props.changed} value={this.props.name}/>
-			</Aux>
+			</React.Fragment>
 		);
 	}
 }
