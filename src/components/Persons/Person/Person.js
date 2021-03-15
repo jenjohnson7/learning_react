6513229c828cb14a172efd1ac97440b6b25ba670
Jen <medtrakdeveloper1@MedTrakDeveloper1s-MacBook-Pro.local>
2021-03-14 22:25:51 -0400

    use aux component to wrap Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,16 +2,17 @@
 
 import React, {Component} from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux.js'
 
 class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering');
 		return(
-			<div className={classes.Person}>
+			<Aux>
 			<p onClick={this.props.click}>I'm {this.props.name}, I'm {this.props.age} years old, and I have {Math.floor(Math.random() * 30)} pets.</p>
 			<p>{this.props.children}</p>
 			<input type="text" onChange={this.props.changed} value={this.props.name}/>
-			</div>
+			</Aux>
 		);
 	}
 }
